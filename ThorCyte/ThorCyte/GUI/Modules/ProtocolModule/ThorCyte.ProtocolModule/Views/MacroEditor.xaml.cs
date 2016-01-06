52cd654fc283777f5ce21d4a3846c796014a83c7
Zhenhua Wang <dong2gua@hotmail.com>
2016-01-06 18:59:32 +0800

    home cancel

--- ThorCyte/ThorCyte/GUI/Modules/ProtocolModule/ThorCyte.ProtocolModule/Views/MacroEditor.xaml.cs
+++ ThorCyte/ThorCyte/GUI/Modules/ProtocolModule/ThorCyte.ProtocolModule/Views/MacroEditor.xaml.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
